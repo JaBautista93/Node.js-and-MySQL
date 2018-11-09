8f213c1a61379c86c434bfd47ebb3e8f97df25f9
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2018-11-08 21:21:16 -0600

    updated

---  bamazon.sql
+++  bamazon.sql
@@ -6,45 +6,45 @@ USE bamazonDB;
 
 CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(45) NULL,
-  department_name VARCHAR(45) NULL,
+  pizza VARCHAR(45) NULL,
+  topping VARCHAR(45) NULL,
   price DECIMAL(10,2) NULL,
-  stock_quantity INT NULL,
+  pizza_quantity DECIMAL (10,2) NULL,
   PRIMARY KEY(id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("Cheese", "Single Topping", 10.50, 500);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("Pepperoni", "Single Topping", 13.50, 500);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("sausage", "Single Topping", 13.50, 350);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("Canadian Bacon", "Single Topping", 14.50, 250);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("2 Vegtable", "Double Topping", 12.50, 350);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("3 Vegtable", "Triple Topping", 13.50, 350);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("White Pie", "Speciality", 15.50, 125);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("Grandma", "Speciality", 14.50, 125);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("BBQ Chicken", "Speciality", 15.50, 125);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("Bufflo Chicken", "Speciality", 15.50, 125);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Supreme", "Speciality", 5.10, 250);
+INSERT INTO products (pizza, topping, price, pizza_quantity)
+VALUES ("Supreme", "Speciality", 15.10, 250);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (pizza, topping, price, pizza_quantity)
 VALUES ("Bake Ziti", "Speciality", 15.25, 100);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,69 +16,147 @@ var connection = mysql.createConnection({
   database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+// connect to the mysql server and sql database
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   afterConnection();
 });
 
 function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     console.log(res);
     connection.end();
   });
 }
+
 function runStore() {
-    inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to order?",
-        choices: [
-          "What type of pizza would you like to buy?",
-          "How many pies?",
-        ]
-      })
-      .then(function(answer) {
-        switch (answer.action) {
-        case "What type of pizza would you like to buy?":
+  inquirer
+    .prompt({
+      name: "orderorLeave",
+      type: "rawlist",
+      message: "Would you like to [ORDER] pizza or [CREATE] your own pie?",
+      choices: [
+        "ORDER",
+        "CREATE",
+      ]
+    })
+    .then(function (answer) {
+      // based on their answer, either call the bid or the post functions
+      switch (answer.action) {
+        case "ORDER":
           buyPizza();
           break;
-  
-        case "How many pies?":
-          howmanyPizza();
+
+        case "CREATE":
+          // howmanyPizza();
+          createPie()
           break;
+      }
+    });
+}
+// fuction to buy Pizza 
+function buyPizza() {
+  // we need to query all the pizza types available to buy
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+    // we should have the items and prompt the customer for the type of pizza they want to buy
+    inquirer
+      .prompt([{
+          name: "Choose",
+          type: "rawlist",
+          choice: function () {
+            var pizzaArray = [];
+            for (var i = 0; i < results.length; i++) {
+              pizzaArray.push(results[i].pizza);
+            }
+            return pizzaArray;
+          },
+          message: "What do you want to order my lil Piasan, I don't got all day"
+        },
+        {
+          name: "cash",
+          type: "input",
+          message: "Do you have enough cash or do I have to have my two friends talk to you out back?"
+        }
+      ])
+      .then(function (answer) {
+        // this gets the info of the pie that is choosen
+        var chosenPie;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].pizza === answer.choice) {
+            chosenPie === results[i];
+          }
+        }
+        // does this Paisan have enough cash for their pizza??
+        if (chosenPie.price < parseInt(answer.cash)) {
+          // if the paisan got the cash...update the database, let the them know whats up and grab the next customer
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [{
+                price: answer.cash
+              },
+              {
+                id: chosenPie.id
+              }
+            ],
+            function (error) {
+              if (error) throw err;
+              console.log("Your money is good here...Here is your pie");
+              runStore();
+            }
+          );
+        } else {
+          // you didn't have enough cash, beat the customer up for wasteing your time
+          console.log("Listen you don't have enough money for the Pizza, get out of here before I introduce you to my cousins")
+          runStore();
         }
       });
-  }
-  
-  function buyPizza() {
-    inquirer
-      .prompt({
-        name: "Pizza Type",
-        type: "input",
-        message: "What type of Pizza would you like to Order?"
-      })
-      .then(function(answer) {
-        var query = "SELECT * FROM bamazon WHERE ?";
-        connection.query(query, { product_name: answer.product_name }, function(err, res) {
-          for (var i = 0; i < res.length; i++) {
-            console.log("Position: " + res[i].id + " || Pizza Type: " + res[i].product_name + " || department_name: " + res[i].price);
-          }
-          runStore();
-        });
-      });
-  }
-  
-  function howmanyPizza() {
-    var query = "SELECT product_name FROM bamazon GROUP BY artist HAVING count(*) > 1";
-    connection.query(query, function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].artist);
-      }
-      runStore();
-    });
-  }
+  });
+}
 
-  runStore()
+
+function createPie() {
+  // prompt for info about the item being put up for auction
+  inquirer
+    .prompt([{
+        name: "Pizza",
+        type: "input",
+        message: "What type of Pizza do you want?"
+      },
+      {
+        name: "Topping",
+        type: "input",
+        message: "What toppings do you want on your pizza?"
+      },
+      {
+        name: "Quanity",
+        type: "input",
+        message: "How many pies do you want?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function (answer) {
+      // when finished prompting, insert a new item into the db with that info
+      connection.query(
+        "INSERT INTO products SET ?", {
+          pizza: answer.Pizza,
+          topping: answer.Topping,
+          price: "18.00",
+          pizza_quantity: answer.Quantity,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Your Pizza masterpiece was made with love Piasan!!");
+          // re-prompt the user for if they want to bid or post
+          runStore();
+        }
+      );
+    });
+}
